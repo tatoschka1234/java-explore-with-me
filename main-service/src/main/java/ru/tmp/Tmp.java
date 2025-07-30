c96bfb542fab2ca1f3fa62532fe2ddc5593d7981
tachen <tatoschka1234@users.noreply.github.com>
2025-07-29 21:46:42 -0700

    upd

---
+++ main-service/src/main/java/ru/tmp/Tmp.java
@@ -0,0 +1,9 @@
+package ru.tmp;
+
+public class Tmp {
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            Thread.sleep(1000);
+        }
+    }
+}
