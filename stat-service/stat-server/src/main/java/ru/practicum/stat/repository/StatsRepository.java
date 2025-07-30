6206f44d85f5308e00cabf61faf0cba8ab24225c
tachen <tatoschka1234@users.noreply.github.com>
2025-07-29 22:42:35 -0700

    sort

--- stat-service/stat-server/src/main/java/ru/practicum/stat/repository/StatsRepository.java
+++ stat-service/stat-server/src/main/java/ru/practicum/stat/repository/StatsRepository.java
@@ -15,14 +15,14 @@ public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
 
     @Query("SELECT new ru.practicum.stat.dto.ViewStatsDto(h.app, h.uri, COUNT(DISTINCT h.ip)) " +
             "FROM EndpointHit h WHERE h.timestamp BETWEEN :start AND :end AND h.uri IN :uris " +
-            "GROUP BY h.app, h.uri")
+            "GROUP BY h.app, h.uri ORDER BY COUNT(DISTINCT h.ip) DESC")
     List<ViewStatsDto> getUniqueStats(@Param("start") LocalDateTime start,
                                       @Param("end") LocalDateTime end,
                                       @Param("uris") List<String> uris);
 
     @Query("SELECT new ru.practicum.stat.dto.ViewStatsDto(h.app, h.uri, COUNT(h)) " +
             "FROM EndpointHit h WHERE h.timestamp BETWEEN :start AND :end AND h.uri IN :uris " +
-            "GROUP BY h.app, h.uri")
+            "GROUP BY h.app, h.uri ORDER BY COUNT(h) DESC")
     List<ViewStatsDto> getAllStats(@Param("start") LocalDateTime start,
                                    @Param("end") LocalDateTime end,
                                    @Param("uris") List<String> uris);
